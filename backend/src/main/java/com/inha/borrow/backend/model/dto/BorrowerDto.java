59e5ffa221e26142ec49d1016b78e89df815d912
형민재 <9512hsmjjs@gmail.com>
2025-07-28 21:26:10 +0900

    fix:DB 세팅 쿼리 오류 수정   - admin_role 테이블 role 컬럼 unique 설정 추가   - 스키마 생성 쿼리 추가

---
+++ backend/src/main/java/com/inha/borrow/backend/model/dto/BorrowerDto.java
@@ -0,0 +1,18 @@
+package com.inha.borrow.backend.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class BorrowerDto {
+    String id;
+    String password;
+    String email;
+    String name;
+    String phonenumber;
+    String studentNumber;
+    String accountNumber;
+}
