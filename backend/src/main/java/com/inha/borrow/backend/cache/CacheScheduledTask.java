b3d968c299797631e7bc706e612dcd83baebc9d9
= <jiwang0917@naver.com>
2025-08-04 11:53:22 +0900

    refactor: CacheScheduledTask 로깅추가 및 상수 고정화

--- backend/src/main/java/com/inha/borrow/backend/cache/CacheScheduledTask.java
+++ backend/src/main/java/com/inha/borrow/backend/cache/CacheScheduledTask.java
@@ -4,14 +4,16 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Component
 @AllArgsConstructor
+@Slf4j
 public class CacheScheduledTask {
     private final IdCache idCache;
     private final SignUpSessionCache signUpSessionCache;
     private final SMSCodeCache smsCodeCache;
-    private final int ONE_HOUR = 3_600_000;
+    private static final int ONE_HOUR = 3_600_000;
 
     // 시간단위는 ms임, 한시간마다 동작되는 메서드
     @Scheduled(fixedRate = ONE_HOUR)
@@ -19,5 +21,6 @@ public class CacheScheduledTask {
         idCache.removeOldId();
         signUpSessionCache.removeOldSignUpSession();
         smsCodeCache.removeOldSMSCode();
+        log.info("[Scheduled] 캐시 정리작업 수행");
     }
 }
