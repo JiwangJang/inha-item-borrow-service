b1c0d9e15fddf5cb47abc45ebd2aafd62ce3f207
= <jiwang0917@naver.com>
2025-09-07 21:14:32 +0900

    fix:Service테스트에 Transactional 어노테이션 안붙은것들 붙여줌

--- backend/src/test/java/com/inha/borrow/backend/service/BorrowerServiceTest.java
+++ backend/src/test/java/com/inha/borrow/backend/service/BorrowerServiceTest.java
@@ -16,11 +16,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.transaction.annotation.Transactional;
+
 import java.util.List;
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
+@Transactional
 class BorrowerServiceTest {
 
     @Autowired
