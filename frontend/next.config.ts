738ff7a3a2e2609b57c9a1d41e9c67c11c1cdfa1
Jiwang <jiwang0917@naver.com>
2026-03-02 11:04:38 +0900

    fix: 수용가능 사진용량 증가

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     devIndicators: false,
+    experimental: {
+        proxyClientMaxBodySize: 10 * 1024 * 1024, // 10MB
+    },
 };
 
 export default nextConfig;
