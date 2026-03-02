1ff317f6b3024aeabecb7dd900fed57050470324
Jiwang <jiwang0917@naver.com>
2026-03-02 10:47:31 +0900

    api server 주소수정

--- frontend/apiServer.ts
+++ frontend/apiServer.ts
@@ -1,5 +1,6 @@
 // 도메인 입력하면 됨
 // const API_SERVER = "https://api-inha-borrow.duckdns.org";
 // const API_SERVER = "http://localhost:8080";
-const API_SERVER = "http://localhost:3000/api";
+// const API_SERVER = "http://localhost:3000/api";
+const API_SERVER = "https://inha-item-borrow-service.vercel.app";
 export default API_SERVER;
