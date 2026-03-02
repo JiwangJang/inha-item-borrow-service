3a23506fccd3398400324859c816d6688e4bda6d
Jiwang <jiwang0917@naver.com>
2026-03-02 10:49:20 +0900

    api server 주소 오탈자 수정

--- frontend/apiServer.ts
+++ frontend/apiServer.ts
@@ -2,5 +2,5 @@
 // const API_SERVER = "https://api-inha-borrow.duckdns.org";
 // const API_SERVER = "http://localhost:8080";
 // const API_SERVER = "http://localhost:3000/api";
-const API_SERVER = "https://inha-item-borrow-service.vercel.app";
+const API_SERVER = "https://inha-item-borrow-service.vercel.app/api";
 export default API_SERVER;
